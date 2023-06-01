91389b22f105209451dd1893e939bfe8789d5e9f
ianK-code <kian99564@gmail.com>
2023-06-01 23:28:51 +0300

    learnt url method basics

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -1,6 +1,9 @@
+const { Console } = require("console");
 const url = require("url");
 
-const myUrl = new URL("http://mywebsite.com:8000/hello.html?id=100&status=active");
+const myUrl = new URL(
+  "http://mywebsite.com:8000/hello.html?id=100&status=active"
+);
 
 // Serialized URL
 console.log(myUrl.href);
@@ -9,5 +12,21 @@ console.log(myUrl.toString());
 // Host (root domain)
 console.log(myUrl.host);
 
-// Hostname (does not get p0rt)
+// Hostname (does not get port)
 console.log(myUrl.hostname);
+
+// Pathname
+console.log(myUrl.pathname);
+
+// Serialized query
+console.log(myUrl.search);
+
+// Params object
+console.log(myUrl.searchParams);
+
+// Add params
+myUrl.searchParams.append("abc", "123");
+console.log(myUrl.searchParams);
+
+// Loop through
+myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`));
